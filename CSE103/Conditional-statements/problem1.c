5991887b284a037fc9db59802e641cb9d334d8cc
Jahidul Islam <80031423+jahiidev@users.noreply.github.com>
2026-06-04 03:39:59 +0600

    Add files via upload

---
+++ CSE103/Conditional-statements/problem1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int x;
+
+    scanf("%d", &x);
+
+    if (x >= 0)
+    {
+        printf("Non-negative");
+    }
+    else
+    {
+        printf("Negative");
+    }
+}
